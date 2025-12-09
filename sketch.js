90a31f07f3ce068daee6537c546c97dcac5f00c7
tomaswallage <thomaswallage@icloud.com>
2025-12-09 17:19:00 +0000

    making it sick v.1
    come onnnnn

--- sketch.js
+++ sketch.js
@@ -4,28 +4,29 @@ function setup() {
 
 function draw() {
   //make background black (done)
-  background(0);
+  background(0, 20);
+  noStroke();
 
-fill(mouseX/3, 0, mouseX/2);
- rect(50, 50,50);
- rect(100, 100,50);
- rect(150, 150,50);
- rect(200, 200,50);
-  //control their colour with mouse (done)
+  fill(mouseX / 3, 0, mouseX / 2);
+  rect(150, 600, 50);
+  rect(400, 450, 50);
+  rect(300, 300, 50);
+  rect(200, 700, 50);
+
+  for (let i = 0; i < 10; i++) {
+    rect(random(windowWidth), random(windowHeight), random(200), random(200));
+  }
 
   //give me a circle that expads with mouse movement
   // give text of your least favourite word in pink!
   let w = mouseX;
-  ellipse(200,200,w);
+  let size = 1000 * tan(radians(frameCount));
 
-  
-  
+  ellipse(windowWidth / 2, windowHeight / 2, size);
 
-  
   textSize(22);
-  fill('pink');
-  text('143, 0, 255', 20, 300);
-  
-  
+  fill("pink");
 
+  text("143, 0, 255", 20, 300);
+  text(sin(radians(frameCount)), 20, 400);
 }
